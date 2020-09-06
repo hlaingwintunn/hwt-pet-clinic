242b1d95dbf5f1370d8d30a7887ae39eb283489f
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-07 00:14:32 +0800

    init commit

---
+++ src/main/java/com/springframe5/hwtpetclinic/HwtPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframe5.hwtpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwtPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwtPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframe5/hwtpetclinic/HwtPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframe5.hwtpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwtPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
