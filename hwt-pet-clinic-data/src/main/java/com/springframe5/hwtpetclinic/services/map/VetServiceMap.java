329e7d4c7e5cdd67a16c6114cf194668c2649339
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-10 00:21:39 +0800

    Implementing Map abstract Service. Close #6.

---
+++ hwt-pet-clinic-data/src/main/java/com/springframe5/hwtpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.springframe5.hwtpetclinic.services.map;
+
+import com.springframe5.hwtpetclinic.model.Vet;
+import com.springframe5.hwtpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+}
