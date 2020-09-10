8cae85b6559616bd7d90b20cf58b8a8d2e6e172d
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-11 00:00:39 +0800

    adding vets and owner page an controller. Close #7 #13

---
+++ hwt-pet-clinic-web/src/main/java/com/springframe5/hwtpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.springframe5.hwtpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String ownerLists() {
+        return "owners/index";
+    }
+}
