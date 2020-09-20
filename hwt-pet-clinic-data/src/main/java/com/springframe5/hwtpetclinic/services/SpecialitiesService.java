941cfbc7ff8aa9c801f7416426d8bf527936f2b0
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-20 13:16:32 +0800

    Added Speciality Map Serice Impl. Close #31.

---
+++ hwt-pet-clinic-data/src/main/java/com/springframe5/hwtpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.springframe5.hwtpetclinic.services;
+
+import com.springframe5.hwtpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
