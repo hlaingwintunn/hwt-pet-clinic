0c915e344ae1b94991ad092c6ef8b7f8004e793b
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-21 17:24:35 +0800

    Convert Owner, Pet and PetType to JPA Entity. Close #36.

--- hwt-pet-clinic-data/src/main/java/com/springframe5/hwtpetclinic/model/Person.java
+++ hwt-pet-clinic-data/src/main/java/com/springframe5/hwtpetclinic/model/Person.java
@@ -1,8 +1,14 @@
 package com.springframe5.hwtpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstname;
+    @Column(name = "last_name")
     private String lastname;
 
     public String getFirstname() {
