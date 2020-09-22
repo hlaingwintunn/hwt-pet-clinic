bb4385a51cd217ee6017eae877f8d9ddb7bf3f52
hlaingwintunn <hlaingwintunn@gmail.com>
2020-09-23 00:33:19 +0800

    Refactoring for project Lombok. Close #51.

--- hwt-pet-clinic-data/src/main/java/com/springframe5/hwtpetclinic/model/Owner.java
+++ hwt-pet-clinic-data/src/main/java/com/springframe5/hwtpetclinic/model/Owner.java
@@ -1,12 +1,34 @@
 package com.springframe5.hwtpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+    @Builder
+    public Owner(String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
+
+    public Owner(String address, String city, String telephone, Set<Pet> pets) {
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
+
     @Column(name = "address")
     private String address;
 
