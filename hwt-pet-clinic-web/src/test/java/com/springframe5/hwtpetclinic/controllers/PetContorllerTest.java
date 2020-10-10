0436db0e9f7d8e0be295ec473bbdb22093b44d0b
hlaingwintunn <hlaingwintunn@gmail.com>
2020-10-11 00:34:48 +0800

    Added create Pet Form. #56

---
+++ hwt-pet-clinic-web/src/test/java/com/springframe5/hwtpetclinic/controllers/PetContorllerTest.java
@@ -0,0 +1,85 @@
+package com.springframe5.hwtpetclinic.controllers;
+
+import com.springframe5.hwtpetclinic.model.Owner;
+import com.springframe5.hwtpetclinic.model.PetType;
+import com.springframe5.hwtpetclinic.services.OwnerService;
+import com.springframe5.hwtpetclinic.services.PetService;
+import com.springframe5.hwtpetclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class PetContorllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    PetContorller petContorller;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(1l).build();
+
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).name("Dog").build());
+        petTypes.add(PetType.builder().id(2L).name("Cat").build());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(petContorller).build();
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(get("/owners/1/pets/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+}
